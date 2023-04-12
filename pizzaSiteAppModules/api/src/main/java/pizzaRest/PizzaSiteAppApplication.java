e739acf292a370c59c1505ba5373c262e403d255
Bugaienko <sergiibugaienko@gmail.com>
2023-04-12 21:51:08 +0200

    2 modules

---
+++ pizzaSiteAppModules/api/src/main/java/pizzaRest/PizzaSiteAppApplication.java
@@ -0,0 +1,20 @@
+package pizzaRest;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PizzaSiteAppApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(PizzaSiteAppApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}

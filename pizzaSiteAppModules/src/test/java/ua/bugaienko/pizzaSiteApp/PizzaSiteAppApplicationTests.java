728dc51ac040731d209c39135d0a96e5ca609e78
Bugaienko <sergiibugaienko@gmail.com>
2023-04-12 17:55:41 +0200

    pizza app added - in dev

---
+++ pizzaSiteAppModules/src/test/java/ua/bugaienko/pizzaSiteApp/PizzaSiteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.bugaienko.pizzaSiteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaSiteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
